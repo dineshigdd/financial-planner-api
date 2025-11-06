2b5c6e11d3286ffb5d5d0822627669dcd7a8f063
Dinesh <dineshigdd@gmail.com>
2025-11-05 21:52:29 -0800

    fix an error in getAllUsers

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -24,7 +24,7 @@ export class UserController {
 
     @Get()
     async findAll() {
-        return this.userService.getAllUsers
+        return this.userService.getAllUsers()
     }
 
     @Get(':id')
